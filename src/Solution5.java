5f7fec5924262159a2adb07d8bd334f814d6c08f
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-04-18 13:27:08 -0700

    HR Solution5

---
+++ src/Solution5.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class Solution5 {
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+//            int a = in.nextInt();
+            int a = 0;
+            int b = 2;
+            int n = 10;
+            int result = a;
+            int c = 1;
+
+            for(int j=1; j<=n; j++) {
+                result = result + (c * b);
+                System.out.print(result+ " ");
+                c = c*2;
+
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
