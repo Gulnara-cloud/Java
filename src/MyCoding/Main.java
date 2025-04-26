0eb09d253fcff816a604084f151fda8463f6ac19
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-04-25 17:11:12 -0700

    MyCoding

---
+++ src/MyCoding/Main.java
@@ -0,0 +1,16 @@
+package MyCoding;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int marks = 48;
+
+        if (48 >= 50) {
+            System.out.println("Passed");
+        } else {
+            System.out.println("Failed");
+        }
+    }
+}
