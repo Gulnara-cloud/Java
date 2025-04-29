3a32c46a9f6ff9f20da1e5927b3c745e0c9ec1d6
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-04-29 15:35:00 -0700

    Task292-293

---
+++ src/JavaIntensive/Task292.java
@@ -0,0 +1,21 @@
+package JavaIntensive;
+
+import java.util.Scanner;
+
+public class Task292 {
+    public static void main(String[] args) {
+        //  Входные данные
+        //Даны два целых числа, каждое число записано в отдельной строке.
+        //
+        //Выходные данные
+        //Выведите наибольшее из данных чисел.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a > b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+}
---
+++ src/JavaIntensive/Task293.java
@@ -0,0 +1,21 @@
+package JavaIntensive;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Task293 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > b) {
+            System.out.println(1);
+        } else if
+            (a < b) {
+            System.out.println(2);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
