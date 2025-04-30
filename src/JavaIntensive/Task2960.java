49d886106e6303552d6e6557a0a06fcc95f6e23c
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-04-30 12:59:34 -0700

    Task2960.java

---
+++ src/JavaIntensive/Task2960.java
@@ -0,0 +1,16 @@
+package JavaIntensive;
+
+import java.util.Scanner;
+
+public class Task2960 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a == 1 & b == 1 | a != 1 & b != 1) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
