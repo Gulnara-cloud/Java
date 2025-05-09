43a4bd0c6ae55960c963dc161b09b588e5991603
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-05-09 16:28:34 -0700

    loops.java

---
+++ src/basics/loops.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class loops {
+    public static void main(String[] args) {
+        for (int i = 5; i > 0; i--)
+            System.out.println("Hello Gulnara " + i);
+    }
+}
+
