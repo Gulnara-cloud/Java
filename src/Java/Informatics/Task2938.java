3ae6c7e71786fb1d0b49f45bec08ba123c237b5e
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-06-14 18:36:51 -0700

    Task2938.java

---
+++ src/Java/Informatics/Task2938.java
@@ -0,0 +1,23 @@
+package Java.Informatics;
+
+import java.util.Scanner;
+
+public class Task2938 {
+    /* Дележ яблок - 1
+N школьников делят K яблок поровну,
+неделящийся остаток остается в корзинке.
+Сколько яблок достанется каждому школьнику?
+
+Входные данные
+Программа получает на вход числа N и K.
+
+Выходные данные
+Программа должна вывести искомое количество яблок.
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+    }
+}
