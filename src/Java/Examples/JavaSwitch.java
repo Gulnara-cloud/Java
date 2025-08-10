66daafb7611b4bd27919c499bbfbb81af39461a0
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-08-10 11:06:07 -0700

    JavaSwitch

---
+++ src/Java/Examples/JavaSwitch.java
@@ -0,0 +1,15 @@
+package Java.Examples;
+
+public class JavaSwitch {
+    public static void main(String[] args) {
+        int day = 2;
+        switch (day) {
+            case 1:
+                System.out.println("Saturday");
+                break;
+            case 2:
+                System.out.println("Sunday");
+                break;
+        }
+    }
+}
