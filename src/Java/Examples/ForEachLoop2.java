0e9c6fa937da39e5c531f79010806be68d05993f
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-08-09 21:11:30 -0700

    ForEachLoop2

---
+++ src/Java/Examples/ForEachLoop2.java
@@ -0,0 +1,10 @@
+package Java.Examples;
+
+public class ForEachLoop2 {
+    public static void main(String[] args) {
+        int[] myNumbers = {1, 2, 3, 4, 5};
+        for (int i : myNumbers) {
+            System.out.println(i);
+        }
+    }
+}
