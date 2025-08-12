e23f1fd16e667e2f400275de662cc1514d2a3968
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-08-11 21:08:42 -0700

    JavaTernaryOp

---
+++ src/Java/Examples/JavaTernaryOp.java
@@ -0,0 +1,9 @@
+package Java.Examples;
+
+public class JavaTernaryOp {
+    public static void main(String[] args) {
+        int time = 22;
+        String res = (time < 18) ? "Good day." : "Good evening.";
+        System.out.println(res);
+    }
+}
