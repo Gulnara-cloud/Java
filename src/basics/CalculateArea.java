9af623ff4fbeb53fcdf6467533487e12eeafbc98
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-09-09 11:59:26 -0700

    CalculateArea

---
+++ src/basics/CalculateArea.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class CalculateArea {
+    public static double calculateArea(double length, double width) {
+        return length * width;
+    }
+    public static void main(String[] args) {
+        double length = 2.50;
+        double width = 0.50;
+        double result = calculateArea(length, width);
+        System.out.println("Calculate area: " + result);
+    }
+}
