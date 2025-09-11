1819083df1e5590f1594e7fe9f526c1441582b35
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-09-10 18:18:01 -0700

    Obj oriented

--- src/basics/Main.java
+++ src/basics/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         var employee = new Employee();
         employee.baseSalary = 50_000;
-        employee.hourlyRate = 20;
+        employee.hourlyRate = 10;
         int wage = employee.calculateWage(10);
         System.out.println(wage);
     }
