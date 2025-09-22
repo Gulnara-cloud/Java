be60a5681f99d749223ef5170fd0905d6db6e03a
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-09-21 23:04:12 -0700

    Binary examples

--- src/Java/Examples/BinaryPowerOfTwo.java
+++ src/Java/Examples/BinaryPowerOfTwo.java
@@ -2,7 +2,7 @@ package Java.Examples;
 
 public class BinaryPowerOfTwo {
     public static void main(String[] args) {
-        int n = 64;
+        int n = 128;
         System.out.println(n + " -> " + isPowerOfTwo(n));
     }
     public static boolean isPowerOfTwo(int n) {
