60869fb9eb7eaef07683c87ac063680dfe073570
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-10-12 20:13:44 -0700

    array

--- src/Java/Examples/StringArray.java
+++ src/Java/Examples/StringArray.java
@@ -2,7 +2,7 @@ package Java.Examples;
 
 public class StringArray {
     public static void main(String[] args) {
-        String[] cars = {"Volvo", "BMW", "Ford"};
+        String[] cars = {"Volvo", "BMW", "Lexus"};
         System.out.println(cars.length);
     }
 }
