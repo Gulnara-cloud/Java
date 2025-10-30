aa1962af53d99e01d98024ea2ad3a01825220405
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-10-29 19:35:49 -0700

    CalculateArea

--- src/basics/CalculateArea.java
+++ src/basics/CalculateArea.java
@@ -5,7 +5,7 @@ public class CalculateArea {
         return length * width;
     }
     public static void main(String[] args) {
-        double length = 2.50;
+        double length = 3.50;
         double width = 0.50;
         double result = calculateArea(length, width);
         System.out.println("Calculate area: " + result);
