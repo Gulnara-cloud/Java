a1ef068518167981b54a331e1591e9fb866baff2
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-11-02 20:16:32 -0800

    LoopBrake

--- src/Java/Examples/LoopBrake.java
+++ src/Java/Examples/LoopBrake.java
@@ -2,7 +2,7 @@ package Java.Examples;
 
 public class LoopBrake {
     public static void main(String[] args) {
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < 10; i++) {
             if (i == 4) {
                 break;
             }
