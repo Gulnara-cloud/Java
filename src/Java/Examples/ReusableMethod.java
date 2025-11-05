d57efa480b63c52eb7df6d3a4114a1f063160b42
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-11-04 16:39:16 -0800

    ReusableMethod

--- src/Java/Examples/ReusableMethod.java
+++ src/Java/Examples/ReusableMethod.java
@@ -5,13 +5,13 @@ public class ReusableMethod {
 
         String name = "Nara";
         int age = 30;
-        happyBirthday(name);
+        happyBirthday(name,age);
 
     }
-    static void happyBirthday(String name) {
+    static void happyBirthday(String name, int age) {
         System.out.println("Happy birthday to you!");
         System.out.printf("Happy birthday dear %s!\n", name);
-        System.out.println("You are x years old!.");
+        System.out.println("You are " + age + " years old!.");
         System.out.println("Happy birthday to you!");
     }
 }
