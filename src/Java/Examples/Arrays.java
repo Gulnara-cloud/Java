f641f76255840f1fb08863b2903c68bd5681702f
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-12-15 15:07:08 -0800

    Arrays

--- src/Java/Examples/Arrays.java
+++ src/Java/Examples/Arrays.java
@@ -2,17 +2,19 @@ package Java.Examples;
 
 public class Arrays {
     public static void main(String[] args) {
-        int a, b, c, d, e;
-        int[] array = new int[5];
+        int a, b, c, d, e, f;
+        int[] array = new int[6];
         array[0] = 10;
         array[1] = 20;
         array[2] = 30;
         array[3] = 40;
         array[4] = 50;
-        System.out.println(array[0]);
+        array[5] = 60;
+        System.out.println(array[1]);
         System.out.println(array[1]);
         System.out.println(array[2]);
         System.out.println(array[3]);
         System.out.println(array[4]);
+        System.out.println(array[5]);
     }
 }
