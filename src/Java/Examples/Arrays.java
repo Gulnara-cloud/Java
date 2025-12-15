428e8844eb04bba680af339754064cf4a6f02169
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-12-15 15:03:48 -0800

    Arrays

--- src/Java/Examples/Arrays.java
+++ src/Java/Examples/Arrays.java
@@ -8,9 +8,11 @@ public class Arrays {
         array[1] = 20;
         array[2] = 30;
         array[3] = 40;
+        array[4] = 50;
         System.out.println(array[0]);
         System.out.println(array[1]);
         System.out.println(array[2]);
         System.out.println(array[3]);
+        System.out.println(array[4]);
     }
 }
