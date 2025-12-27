bcfc48133f0ac806b0b4af418611981564126f6d
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-12-26 20:56:19 -0800

    LoopContinue

--- src/Java/Examples/LoopContinue.java
+++ src/Java/Examples/LoopContinue.java
@@ -2,7 +2,7 @@ package Java.Examples;
 
 public class LoopContinue {
     public static void main(String[] args) {
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < 14; i++) {
             if (i == 4) {
                 continue;
             }
