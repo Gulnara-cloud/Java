f8a381524f141e76960701f6c3c3d15388e73ae3
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-12-31 10:10:20 -0800

    BinaryTable

--- src/Java/Examples/BinaryTable.java
+++ src/Java/Examples/BinaryTable.java
@@ -2,7 +2,7 @@ package Java.Examples;
 
 public class BinaryTable {
     public static void main(String[] args) {
-        for(int i = 0; i < 20; i++) {
+        for(int i = 0; i < 30; i++) {
             System.out.println(i + " -> " + Integer.toBinaryString(i));
         }
     }
