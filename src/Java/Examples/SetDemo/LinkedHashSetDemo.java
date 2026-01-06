8d7db67cd299338e854f8b42105801e3d478431c
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-01-05 20:52:38 -0800

    LinkedHashSetDemo

--- src/Java/Examples/SetDemo/LinkedHashSetDemo.java
+++ src/Java/Examples/SetDemo/LinkedHashSetDemo.java
@@ -13,7 +13,6 @@ public class LinkedHashSetDemo {
         Set<String> cities = new LinkedHashSet<>();
 
         //Add elements
-        cities.add("Washington DC");
         cities.add("Chicago");
         cities.add("New York");
         cities.add("Los Angeles");
