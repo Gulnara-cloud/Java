db9105a5fc29cc3874771d010dfb8986e244fe5e
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-01-24 23:16:15 -0800

    DuplicateCheck

---
+++ src/Java/Examples/SetDemo/DuplicateCheck.java
@@ -0,0 +1,22 @@
+package Java.Examples.SetDemo;
+
+import java.util.HashSet;
+
+public class DuplicateCheck {
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2};
+        System.out.println(containsDuplicate(arr));
+
+    }
+}
