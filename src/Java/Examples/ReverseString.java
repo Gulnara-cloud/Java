cf7fd7aae6ee72dbc42565be872a33e835b535e2
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-02-03 20:40:44 -0800

    ReverseString

---
+++ src/Java/Examples/ReverseString.java
@@ -0,0 +1,18 @@
+package Java.Examples;
+
+public class ReverseString {
+
+    public static String reverse(String s) {
+        String result = "";
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            result += s.charAt(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse("h e l l o"));
+    }
+}
