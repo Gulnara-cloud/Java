6ed6cc823293f2453d04f2f1b28b1027511eccda
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-02-04 23:40:17 -0800

    ReverseString

--- src/Java/Examples/ReverseString.java
+++ src/Java/Examples/ReverseString.java
@@ -13,6 +13,6 @@ public class ReverseString {
     }
 
     public static void main(String[] args) {
-        System.out.println(reverse("h e l l o"));
+        System.out.println(reverse("los angeles"));
     }
 }
