8c184cad5083f1fdc5d0437c4ea61d60fac6dec2
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-02-20 12:13:25 -0800

    BacktrackingExample

---
+++ src/Java/Examples/BacktrackingExample.java
@@ -0,0 +1,29 @@
+package Java.Examples;
+
+import java.util.*;
+
+public class BacktrackingExample {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> result = new ArrayList<>();
+
+        backtrack(nums, 0, new ArrayList<>(), result);
+
+        System.out.println(result);
+    }
+
+    static void backtrack(int[] nums, int start,
+                          List<Integer> current,
+                          List<List<Integer>> result) {
+
+        result.add(new ArrayList<>(current));
+
+        for (int i = start; i < nums.length; i++) {
+
+            current.add(nums[i]);
+            backtrack(nums, i + 1, current, result);
+            current.remove(current.size() - 1);
+        }
+    }
+}
