e6254598c37159360ffae57eb4d5dee668700737
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-03-08 22:52:12 -0700

    DuplicateCheck

--- src/Java/Examples/SetDemo/DuplicateCheck.java
+++ src/Java/Examples/SetDemo/DuplicateCheck.java
@@ -15,7 +15,7 @@ public class DuplicateCheck {
         return false;
     }
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 2};
+        int[] arr = {1, 2, 3, 2, 3};
         System.out.println(containsDuplicate(arr));
     }
 }
