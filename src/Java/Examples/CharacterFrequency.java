6991a817776a97e8f4ad313dc82aef6afad0a06e
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-03-13 20:46:43 -0700

    CharacterFrequency

---
+++ src/Java/Examples/CharacterFrequency.java
@@ -0,0 +1,21 @@
+package Java.Examples;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CharacterFrequency {
+
+    public static void main(String[] args) {
+
+        String text = "programming";
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : text.toCharArray()) {
+
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        }
+        System.out.println(map);
+    }
+}
