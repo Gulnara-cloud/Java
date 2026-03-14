ad2f57aa5d876cb6fda001c7683fb5d47e1c2aab
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-03-13 20:48:18 -0700

    FirstUnique

---
+++ src/Java/Examples/FirstUnique.java
@@ -0,0 +1,26 @@
+package Java.Examples;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstUnique {
+
+    public static int firstUnique(int[] nums) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int num : nums) {
+            if (map.get(num) == 1) {
+                return num;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 1, 2, 1, 2};
+        System.out.println(firstUnique(nums));
+    }
+}
