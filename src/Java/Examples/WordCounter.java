bdf929b4e23b9ffa5f85c538ec75c9553811635f
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-03-13 20:41:08 -0700

    WordCounter

---
+++ src/Java/Examples/WordCounter.java
@@ -0,0 +1,22 @@
+package Java.Examples;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordCounter {
+
+    public static void main(String[] args) {
+
+        String text = "java is good java is powerful";
+
+        String[] words = text.split(" ");
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        System.out.println(map);
+    }
+}
