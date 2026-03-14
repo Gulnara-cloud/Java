b930f0c0ef7972f59c0944163255a839e0f5757f
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-03-13 20:42:38 -0700

    UserFilter

---
+++ src/Java/Examples/UserFilter.java
@@ -0,0 +1,33 @@
+package Java.Examples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class User {
+
+    String name;
+    int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+public class UserFilter {
+
+    public static void main(String[] args) {
+
+        List<User> users = new ArrayList<>();
+
+        users.add(new User("Anna", 17));
+        users.add(new User("John", 25));
+        users.add(new User("Mike", 20));
+
+        for (User user : users) {
+
+            if (user.age >= 18) {
+                System.out.println(user.name);
+            }
+        }
+    }
+}
