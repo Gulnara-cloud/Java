4f6c1c22f34af090098d2f00acae17fda374b7df
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-03-22 16:24:04 -0700

    Reverse.java

--- src/Java/Examples/Reverse.java
+++ src/Java/Examples/Reverse.java
@@ -2,7 +2,7 @@ package Java.Examples;
 
 public class Reverse {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5};
+        int[] nums = {1, 2, 3, 4, 5,6};
 
         reverse(nums);
 
