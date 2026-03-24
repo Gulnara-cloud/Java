81d1abace5a6c8bafb770a55efd27e14321eb6a2
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-03-23 18:11:54 -0700

    PatternPrinter

---
+++ src/Java/Examples/PatternPrinter.java
@@ -0,0 +1,14 @@
+package Java.Examples;
+
+public class PatternPrinter {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
