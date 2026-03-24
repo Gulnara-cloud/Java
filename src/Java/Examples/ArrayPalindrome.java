e067a661d35026f30a72a80139dc5af09652fa78
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-03-23 18:15:06 -0700

    ArrayPalindrome

---
+++ src/Java/Examples/ArrayPalindrome.java
@@ -0,0 +1,18 @@
+package Java.Examples;
+
+public class ArrayPalindrome {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 1};
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < arr.length / 2; i++) {
+            if (arr[i] != arr[arr.length - 1 - i]) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Is palindrome: " + isPalindrome);
+    }
+}
