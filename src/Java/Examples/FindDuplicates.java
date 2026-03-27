1283a9ff1932bb85bd415866ec6e27af752ce7d6
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-03-27 10:46:37 -0700

    FindDuplicates

---
+++ src/Java/Examples/FindDuplicates.java
@@ -0,0 +1,17 @@
+package Java.Examples;
+
+public class FindDuplicates {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 2, 3, 1};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println("Duplicate: " + arr[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
