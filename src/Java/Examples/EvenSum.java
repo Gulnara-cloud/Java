0850210c683e97cc5add0ca604125848fb9a2ee1
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-03-31 21:10:30 -0700

    EvenSum

---
+++ src/Java/Examples/EvenSum.java
@@ -0,0 +1,17 @@
+package Java.Examples;
+
+public class EvenSum {
+    public static void main(String[] args) {
+        int[] numbers = {5, 8, 12, 3, 7, 10};
+
+        int sum = 0;
+
+        for (int num : numbers) {
+            if (num % 2 == 0 && num > 5) {
+                sum += num;
+            }
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+}
