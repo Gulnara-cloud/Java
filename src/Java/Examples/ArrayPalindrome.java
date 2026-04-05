eb95fcc3692c6cedc8f6072e300af1d70b321ef1
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-04-04 21:00:01 -0700

    ArrayPalindrome

--- src/Java/Examples/ArrayPalindrome.java
+++ src/Java/Examples/ArrayPalindrome.java
@@ -3,7 +3,7 @@ package Java.Examples;
 public class ArrayPalindrome {
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 2, 1};
+        int[] arr = {1, 2, 3, 3, 1,};
         boolean isPalindrome = true;
 
         for (int i = 0; i < arr.length / 2; i++) {
