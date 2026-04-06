58c18cc152ea58df7d3821a1d7f2fc1287cd98b7
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-04-06 16:53:19 -0700

    ReverseArray

--- src/Java/Examples/ReverseArray.java
+++ src/Java/Examples/ReverseArray.java
@@ -3,7 +3,7 @@ package Java.Examples;
 public class ReverseArray {
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 2, 3, 4, 5, 6};
 
         for (int i = 0; i < arr.length / 2; i++) {
             int temp = arr[i];
