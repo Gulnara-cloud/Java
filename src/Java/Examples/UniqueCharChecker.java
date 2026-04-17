3f16d3fb30bfa842658a66b68ea3d7fde9f5b00d
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-04-17 14:47:39 -0700

    UniqueCharChecker

---
+++ src/Java/Examples/UniqueCharChecker.java
@@ -0,0 +1,31 @@
+package Java.Examples;
+
+import java.util.*;
+
+// 4. First Unique Character
+class UniqueCharChecker {
+    public int firstUnique(String s) {
+        Map<Character, Integer> count = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            count.put(ch, count.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (count.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        UniqueCharChecker checker = new UniqueCharChecker();
+
+        int result = checker.firstUnique("leetcode");
+
+        System.out.println(result);
+    }
+}
