bf26933ad0dd5f9e7491eb942b427d5db8d722a3
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-04-23 12:32:46 -0700

    Checker

---
+++ src/Java/Examples/Checker.java
@@ -0,0 +1,24 @@
+package Java.Examples;
+
+import java.util.HashMap;
+
+public class Checker {
+
+    public static boolean hasDuplicates(int[] nums) {
+        HashMap<Integer, Integer> values = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (values.containsKey(nums[i])) {
+                return true;
+            }
+            values.put(nums[i], i);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {1, 2, 3, 4, 2};
+        boolean result = hasDuplicates(nums);
+        System.out.println(result);
+    }
+}
