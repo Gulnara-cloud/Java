81605837df4bae5aeb2cd976b0acd1963d3f1e32
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-04-24 21:53:44 -0700

    PrintFirstDuplicate

---
+++ src/Java/Examples/PrintFirstDuplicate.java
@@ -0,0 +1,27 @@
+package Java.Examples;
+
+import java.util.HashSet;
+
+public class PrintFirstDuplicate {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 3, 5, 3, 2, 6};
+
+        findFirstDuplicate(numbers);
+    }
+
+    public static void findFirstDuplicate(int[] numbers) {
+
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : numbers) {
+            if (seen.contains(num)) {
+                System.out.println("First duplicate: " + num);
+                return;
+            }
+            seen.add(num);
+        }
+        System.out.println("No duplicates found");
+    }
+}
