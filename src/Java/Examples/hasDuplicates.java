70ae3eb155ff814166e62434e447602b73c624e5
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-04-25 19:02:42 -0700

    hasDuplicates

---
+++ src/Java/Examples/hasDuplicates.java
@@ -0,0 +1,26 @@
+package Java.Examples;
+
+import java.util.Arrays;
+
+public class hasDuplicates {
+
+    public static boolean hasDuplicate(int[] numbers) {
+        Arrays.sort(numbers); // sort array first
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] == numbers[i + 1]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 2};
+
+        boolean result = hasDuplicate(numbers);
+
+        System.out.println(result);
+    }
+}
