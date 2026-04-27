69cfc51bfa57114ed819ff16156e687a5451ee4d
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-04-27 14:49:45 -0700

    MatrixExample

--- src/Java/Examples/MatrixExample.java
+++ src/Java/Examples/MatrixExample.java
@@ -5,7 +5,7 @@ public class MatrixExample {
         int[][] matrix = {
                 {1, 2, 3},
                 {4, 5, 6},
-                {7, 8, 9}
+                {7, 8, 7}
         };
 
         for (int i = 0; i < matrix.length; i++) {
