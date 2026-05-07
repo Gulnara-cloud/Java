e6c62b1987cbcca298d96570dede449956552b06
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-05-06 17:00:15 -0700

    ArrayExample

---
+++ src/Java/Examples/ArrayExample.java
@@ -0,0 +1,22 @@
+package Java.Examples;
+
+import java.util.Arrays;
+
+public class ArrayExample {
+
+    public static int findSecondLargest(int[] numbers) {
+
+        Arrays.sort(numbers);
+
+        return numbers[numbers.length - 2];
+    }
+
+    public static void main(String[] args) {
+
+        int[] numbers = {12, 5, 8, 21, 19};
+
+        int result = findSecondLargest(numbers);
+
+        System.out.println(result);
+    }
+}
