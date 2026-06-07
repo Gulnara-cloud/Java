77e4664a94a5eeb2090c38768d8a48042a84c492
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-06-07 16:20:07 -0700

    MoveZerosExample

---
+++ src/Java/Examples/MoveZerosExample.java
@@ -0,0 +1,26 @@
+package Java.Examples;
+
+import java.util.Arrays;
+
+public class MoveZerosExample {
+    public static void main(String[] args) {
+
+        int[] numbers = {0, 1, 0, 3, 12};
+
+        int index = 0;
+
+        for (int number : numbers) {
+            if (number != 0) {
+                numbers[index] = number;
+                index++;
+            }
+        }
+
+        while (index < numbers.length) {
+            numbers[index] = 0;
+            index++;
+        }
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
