e1f0c69d447352c6ce78fc0f9d2186d3c705a123
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-06-09 10:40:15 -0700

    TwoSumExample

---
+++ src/Java/Examples/TwoSumExample.java
@@ -0,0 +1,21 @@
+package Java.Examples;
+
+public class TwoSumExample {
+    public static void main(String[] args) {
+
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+
+                if (numbers[i] + numbers[j] == target) {
+                    System.out.println("Indexes: " + i + ", " + j);
+                    return;
+                }
+            }
+        }
+
+        System.out.println("No pair found");
+    }
+}
