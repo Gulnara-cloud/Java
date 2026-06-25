fc746b69f4c891afe5a3d545434aeaac6cc9d916
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-06-24 22:58:55 -0700

    MoveZerosExample

--- src/Java/Examples/MoveZerosExample.java
+++ src/Java/Examples/MoveZerosExample.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class MoveZerosExample {
     public static void main(String[] args) {
 
-        int[] numbers = {0, 1, 0, 3, 12};
+        int[] numbers = {0, 1, 2, 0, 12};
 
         int index = 0;
 
