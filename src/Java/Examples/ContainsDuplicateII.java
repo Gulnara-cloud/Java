a35378dbb24a089278255c3d0b72c803efee611d
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-07-07 22:24:43 -0700

    ContainsDuplicateII

--- src/Java/Examples/ContainsDuplicateII.java
+++ src/Java/Examples/ContainsDuplicateII.java
@@ -27,7 +27,7 @@ public class ContainsDuplicateII {
 
     public static void main(String[] args) {
 
-        int[] nums = {1, 2, 3, 1};
+        int[] nums = {1, 2, 3, 1, 5};
 
         System.out.println(containsNearbyDuplicate(nums, 3));
     }
