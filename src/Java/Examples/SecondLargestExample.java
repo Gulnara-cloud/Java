c74f5dcf3b6e570a3e96a0b3a2c64d4100354cf0
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-07-21 10:53:08 -0700

    SecondLargestExample

--- src/Java/Examples/SecondLargestExample.java
+++ src/Java/Examples/SecondLargestExample.java
@@ -3,8 +3,7 @@ package Java.Examples;
 public class SecondLargestExample {
     public static void main(String[] args) {
 
-        int[] numbers = {10, 5, 20, 8, 20, 45};
-
+        int[] numbers = {10, 30, 5, 20, 8, 20, 45};
         int largest = Integer.MIN_VALUE;
         int secondLargest = Integer.MIN_VALUE;
 
