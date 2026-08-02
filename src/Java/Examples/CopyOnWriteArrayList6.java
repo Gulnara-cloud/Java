885d30f51aab0e057b8140418f27343d34ddc8e7
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-08-01 21:44:25 -0700

    CopyOnWriteArrayList6

--- src/Java/Examples/CopyOnWriteArrayList6.java
+++ src/Java/Examples/CopyOnWriteArrayList6.java
@@ -14,7 +14,7 @@ public class CopyOnWriteArrayList6 {
         safeList.add("B");
         safeList.add("C");
         safeList.add("D");
-        safeList.add("A");
+        safeList.add("B");
 
         for (String s : safeList) {
             System.out.print(s + " ");
