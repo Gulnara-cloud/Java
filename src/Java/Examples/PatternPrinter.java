a7b5ff8e5f4f595f446bf6625e1e4f57e5f8b3e7
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-08-17 21:08:24 -0700

    PatternPrinter

--- src/Java/Examples/PatternPrinter.java
+++ src/Java/Examples/PatternPrinter.java
@@ -4,7 +4,7 @@ public class PatternPrinter {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 7; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
